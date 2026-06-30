173b249f4dbe667b11aee91f5b18bc5cd4abb95c
oscarargumedo <00088826@uca.edu.sv>
2026-06-29 23:54:15 -0600

    Creando menu de inicio

--- mainproyecto.cpp
+++ mainproyecto.cpp
@@ -2,3 +2,28 @@
 #include <windows.h>
 
 using namespace std;
+
+int opcop = 1, opcion = 1;
+
+               int
+               main()
+{
+    do
+    {
+        cout << "Bienvenido a Cafeteria UCA" << endl
+             << "Seleccione una opcion: " << endl
+             << "1)Realizar pedido" << endl
+             << "2)Salir" << endl;
+        cin >> opcion;
+        if (opcion == 1)
+        {
+            /* code */
+        }
+        else
+        {
+            opcop = 2;
+        }
+
+    } while (opcop == 1);
+    return 0;
+}
